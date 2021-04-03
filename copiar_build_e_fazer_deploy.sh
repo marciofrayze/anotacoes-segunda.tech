662c7a50db3aa3f271ad501742d22de917caca45
Marcio Frayze David <marcio.david@serpro.gov.br>
2021-04-03 12:45:09 -0300

    Diversas novas paginas

--- copiar_build_e_fazer_deploy.sh
+++ copiar_build_e_fazer_deploy.sh
@@ -1,3 +1,5 @@
 rm -rf ./public/*
 cp -r ./vimwiki_html/* ./public
+echo "@media (max-width: 1000px) {  body { margin: 0.5em 2em 0.5em 2em } }" >> ./public/style.css
+echo "body { max-width: 40em; }" >> ./public/style.css
 firebase deploy
